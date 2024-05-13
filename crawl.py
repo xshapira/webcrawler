e69b6db9493caf56c126147f3aac71fb43451563
Max Shapira <max@winoutt.com>
2024-05-13 12:31:23 +0300

    fetch html content from urls

--- crawl.py
+++ crawl.py
@@ -1,3 +1,29 @@
+import requests
+
+from logger import setup_logger
+
+log = setup_logger(__name__)
+
+
+def fetch_html_content(url: str) -> str | None:
+    """
+    Fetch HTML content for a given URL.
+
+    Args:
+        url (str): The URL from which to fetch the HTML content.
+
+    Returns:
+        str: The raw HTML content of the page.
+    """
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text
+    except requests.exceptions.RequestException as exc:
+        log.error(f"Failed to fetch HTML content from {url}: {exc}")
+        return None
+
+
 def main() -> None:
     pass
 
