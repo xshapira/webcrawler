cca3df187c841c5eaf21d88fe29c653232a485da
Max Shapira <max@winoutt.com>
2024-05-13 12:54:59 +0300

    parse html for page urls

--- crawl.py
+++ crawl.py
@@ -1,4 +1,7 @@
+from urllib.parse import urljoin
+
 import requests
+from bs4 import BeautifulSoup as soup
 
 from logger import setup_logger
 
@@ -22,6 +25,33 @@ def fetch_html_content(url: str) -> str | None:
     except requests.exceptions.RequestException as exc:
         log.error(f"Failed to fetch HTML content from {url}: {exc}")
         return None
+
+
+def extract_page_urls(html_content: str, url: str, current_depth: int) -> list[dict]:
+    """
+    Parse HTML content to extract page URLs.
+
+    Args:
+        html_content (str): The raw HTML content.
+        url (str): The URL from which the HTML content was fetched.
+        current_depth (int): The current depth of the URL being processed.
+
+    Returns:
+        A list of dictionaries, where each dictionary contains the
+        following keys:
+        - 'url': The URL of the page.
+        - 'page': The URL of the page where the URL was found.
+        - 'depth': The depth at which the URL was found relative to the starting URL.
+    """
+    return [
+        {
+            "url": urljoin(url, a["href"]),
+            "page": url,
+            "depth": current_depth,
+        }
+        for a in soup(html_content, "html.parser").find_all("a")
+        if "href" in a.attrs
+    ]
 
 
 def main() -> None:
